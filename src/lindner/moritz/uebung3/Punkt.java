c64a3021538f2a75fa491c5f2e6e1e80556806bd
linde9821 <lindner197@gmail.com>
2019-05-28 18:07:09 +0200

    minore changes to Punkt and u5

--- src/lindner/moritz/uebung3/Punkt.java
+++ src/lindner/moritz/uebung3/Punkt.java
@@ -1,32 +1,32 @@
 package lindner.moritz.uebung3;
 
 /**
- * Punkt 
- * Beschreibt einen Punkt.
+ * Punkt Beschreibt einen Punkt.
  * 
  */
 public class Punkt {
-	public static void  iniPunkt() {
+	public static void iniPunkt() {
 		anzahl = 0; // 77
 	}
-	
+
 	public static int anzahl;
-	
+
 	protected double x, y;
-	
+
 	/**
 	 * Constructs and initializes a point at x, y in the coordinate space.
+	 * 
 	 * @param x a double
-	 * @param y a double 
+	 * @param y a double
 	 */
-	public Punkt (double x, double y) {
-		anzahl ++;
+	public Punkt(double x, double y) {
+		anzahl++;
 		System.out.println("Anzahl: " + Punkt.anzahl);
-		
+
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public Punkt() {
 		this(0, 0);
 	}
@@ -54,20 +54,25 @@ public class Punkt {
 	public static double distanz(Punkt a, Punkt b) {
 		double x = a.getX() - b.getX();
 		double y = a.getY() - b.getY();
-		
-		return Math.sqrt((x*x) + (y*y));
+
+		return Math.sqrt((x * x) + (y * y));
 	}
-	
+
 	public void translate(double x, double y) {
 		this.x += x;
 		this.y += y;
 	}
-	
+
 	public void translate(Punkt p) {
 		this.translate(p.getX(), p.getY());
 	}
-	
+
 	public double flaeche() {
 		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + "|" + y + ")";
 	}
 }
--- src/lindner/moritz/uebung5/HashTest.java
+++ src/lindner/moritz/uebung5/HashTest.java
@@ -1,7 +1,11 @@
 package lindner.moritz.uebung5;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import lindner.moritz.uebung3.Punkt;
 
 public class HashTest {
 	Set<Float> hashSet = new HashSet<Float>();
@@ -9,13 +13,23 @@ public class HashTest {
 	public HashTest() {
 		for (int i = 5; i >= 0; i--)
 			hashSet.add((float) i * i);
-		
+
 		hashSet.add(3.5f);
 		hashSet.add(null);
 	}
 
 	public static void main(String... args) {
-		HashTest hashSetSample = new HashTest();
-		System.out.println(hashSetSample.hashSet);
+		/*
+		 * HashTest hashSetSample = new HashTest();
+		 * System.out.println(hashSetSample.hashSet);
+		 */
+
+		List<Punkt> punktListe = new ArrayList<Punkt>();
+
+		for (int i = 0; i < 10; i++) {
+			punktListe.add(new Punkt(i, Math.sqrt(i)));
+		}
+		
+		System.out.println(punktListe);
 	}
 }
